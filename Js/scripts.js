f4c797ba16b4480a9919adc70394310bb744f796
Griffin1770 <159955963+Griffin1770@users.noreply.github.com>
2024-06-13 19:12:08 +0200

    popup works
    finally

--- Js/scripts.js
+++ Js/scripts.js
@@ -29,6 +29,129 @@ showSlide(currentIndex);
 
 
 
+let productsF = {
+    data: [
+      {
+        productName: "The Classic Mars Experience",
+        category: "The Oppulence",
+        price: "R25000",
+        image: "flightMars.png",
+      },
+      {
+        productName: "Neptune's Cosmic Dive",
+        category: "BThe Voyagerr",
+        price: "R45000",
+        image: "flightNeptune.png",
+      },
+      {
+        productName: "The Giant's Journey",
+        category: "The Odyssey",
+        price: "R65000",
+        image: "flightJupiter.png",
+      },
+      {
+        productName: "Ring Around a Saturn",
+        category: " The Explorer",
+        price: "R40000",
+        image: "flightSaturn.png",
+      },
+      {
+        productName: "Uranus Uncharted",
+        category: "The Endeavor",
+        price: "R50000",
+        image: "flightUranus.png",
+      },
+      {
+        productName: "Moonlight Meander",
+        category:  "The Voyager",
+        price: "R15000",
+        image: "flightMoon.png",
+      },
+    ],
+  };
+  for (let i of productsF.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    card.appendChild(container);
+    document.getElementById("productsF").appendChild(card);
+  }
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("activeSearch");
+      } else {
+        button.classList.remove("activeSearch");
+      }
+    });
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("searchInput").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+
+
+
+
+
 let form = document.forms["contact"];
 
 form.addEventListener("submit", openSubmitModal);
@@ -207,3 +330,129 @@ function checkout() {
 }
 
 
+
+
+
+
+
+
+let products = {
+    data: [
+      {
+        productName: "The Classic Mars Experience",
+        category: "The Oppulence",
+        price: "R25000",
+        image: "flightMars.png",
+      },
+      {
+        productName: "Neptune's Cosmic Dive",
+        category: "BThe Voyagerr",
+        price: "R45000",
+        image: "flightNeptune.png",
+      },
+      {
+        productName: "The Giant's Journey",
+        category: "The Odyssey",
+        price: "R65000",
+        image: "flightJupiter.png",
+      },
+      {
+        productName: "Ring Around a Saturn",
+        category: " The Explorer",
+        price: "R40000",
+        image: "flightSaturn.png",
+      },
+      {
+        productName: "Uranus Uncharted",
+        category: "The Endeavor",
+        price: "R50000",
+        image: "flightUranus.png",
+      },
+      {
+        productName: "Moonlight Meander",
+        category:  "The Voyager",
+        price: "R15000",
+        image: "flightMoon.png",
+      },
+    ],
+  };
+  for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("activeSearch");
+      } else {
+        button.classList.remove("activeSearch");
+      }
+    });
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("searchInput").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+
+  
